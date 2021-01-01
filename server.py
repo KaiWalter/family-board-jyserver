898155192cbb2d69eb381745849170307e61d8d1
Kai Walter <kai.walter@outlook.de>
2021-01-01 12:19:31 +0100

    test with Flask

--- server.py
+++ server.py
@@ -4,13 +4,18 @@ import argparse
 import logging
 import time
 
-from jyserver.Server import Client, Server
+import jyserver.Flask as jsf
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
 
 
-class App(Client):
+@jsf.use(app)
+class App:
     def __init__(self):
         pass
 
+    @jsf.task
     def main(self):
         while True:
             self.js.dom.message.innerHTML = "calendar refresh"
@@ -19,15 +24,26 @@ class App(Client):
             time.sleep(15)
 
 
+@app.route('/')
+def index_page(name=None):
+    App.main()
+    return App.render(render_template('index.html'))
+
+
+@app.route('/msatoken')
+def msa_token():
+    return App.render(render_template('msatoken.html'))
+
+
 def parse_args():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--logfile', required=False,
                         dest='logfile', help='Name of log file.', type=str, default='server.log')
     parser.add_argument('-a', '--ip', required=False,
-                        dest='address', help='IP address to host on.', type=str, default=None)
+                        dest='address', help='IP address to host on.', type=str, default='0.0.0.0')
     parser.add_argument('-p', '--port', required=False,
-                        dest='port', help='Port to host on.', type=int, default=80)
+                        dest='port', help='Port to host on.', type=int, default=8080)
     args = parser.parse_args()
 
     return args
@@ -42,7 +58,5 @@ if __name__ == "__main__":
         filename=args.logfile, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 
     # main process
-    httpd = Server(App, ip=args.address, port=args.port, verbose=False)
-    logging.info('serving at port %s', httpd.port)
-    print('ready')
-    httpd.start()
+    logging.info('serving at on address %s port %s', args.address, args.port)
+    app.run(host=args.address, port=args.port)
